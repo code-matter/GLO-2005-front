e357d0dd937e0de44e4c2f211e897de2f06f88cf
Alexandre <alexandre.caissy@norda.com>
2024-03-14 19:21:22 -0400

    feat: plugged a bit of back

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,10 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  const [count, setCount] = useState(0);
-  const navigate = useNavigate();
-  const [fakeCall, setFakeCall] = useState(false);
-
-  useEffect(() => {
-    const fakeFetch = async () => {
-      setTimeout(() => navigate("/Login"), 2000);
-    };
-    if (fakeCall) fakeFetch();
-  }, [fakeCall]);
-
   return (
     <div>
       <h1>VOICI LE PROJET DE GLO-2005 FRONT-END</h1>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: 24,
-        }}
-      >
-        <button onClick={() => setCount(count > 0 ? count - 1 : 0)}>-</button>
-        <h2>{count}</h2>
-        <button onClick={() => setCount(count + 1)}>+</button>
-        <button onClick={() => setFakeCall(true)}>FAKE FETCH</button>
-      </div>
+      <Link to="/login">Connexion</Link>
     </div>
   );
 };
