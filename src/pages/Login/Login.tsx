0b66128e990043e999a3a7719d920e635cb9ed56
Alexandre <alexandre.caissy@norda.com>
2024-03-21 09:08:16 -0400

    feat: added small login info

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,11 +1,17 @@
 import { Button, Form, Input } from "antd";
 import "./login.css";
 import { LoginSignupService } from "../../services/LoginSignup/loginSignup";
+import { useState } from "react";
 
 function Login() {
+  const [user, setUser] = useState<any | null>(null);
+
   const handleSubmitForm = async (formData: unknown) => {
     try {
-      await LoginSignupService.logIn(formData);
+      const data = await LoginSignupService.logIn(formData);
+      const user = await data.json();
+
+      setUser(user[0]);
     } catch (error) {
       console.error(error);
     }
@@ -13,15 +19,23 @@ function Login() {
 
   return (
     <div>
-      <Form layout="vertical" onFinish={handleSubmitForm}>
-        <Form.Item name="email" label="Courriel">
-          <Input />
-        </Form.Item>
-        <Form.Item name="password" label="Mot de passe">
-          <Input />
-        </Form.Item>
-        <Button htmlType="submit">Connexion</Button>
-      </Form>
+      {!user ? (
+        <Form layout="vertical" onFinish={handleSubmitForm}>
+          <Form.Item name="email" label="Courriel">
+            <Input />
+          </Form.Item>
+          <Form.Item name="password" label="Mot de passe">
+            <Input />
+          </Form.Item>
+          <Button htmlType="submit">Connexion</Button>
+        </Form>
+      ) : (
+        <div>
+          <h1>
+            Salut {user.prenom} {user.nom}
+          </h1>
+        </div>
+      )}
     </div>
   );
 }
